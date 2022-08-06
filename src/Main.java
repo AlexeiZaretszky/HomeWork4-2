264471850015813605dbddd9077ff4b1be4730e6
Alexey <ACKOMBAT@yandex.ru>
2022-08-06 21:09:40 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+
+
+        //Задание 1
+
         int clientOS = 1;
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
